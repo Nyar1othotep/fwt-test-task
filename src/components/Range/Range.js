4b54150b3f96f00a5cd78c42a88804191dc66b88
Maxim <freelancerart@yandex.ru>
2023-04-15 14:44:12 +0400

    feat: add styles and media

--- src/components/Range/Range.js
+++ src/components/Range/Range.js
@@ -25,91 +25,95 @@ const Range = ({
    useOutsideClick(ref, hideMenu);
 
    return (
-      <div
-         ref={ref}
-         className={
-            (className ? className : "") +
-            "Range " +
-            (isOpen ? "Range--open " : "")
-         }
-         tabIndex={0}
-         onClick={isOpen ? hideMenu : openMenu}
-         onKeyPress={(e) => {
-            if ((e.key === " " && isOpen) || (e.key === "Enter" && isOpen)) {
-               hideMenu();
-            } else {
-               openMenu();
+      <div className="Range__wrapper">
+         <div
+            ref={ref}
+            className={
+               (className ? className : "") +
+               "Range " +
+               (isOpen ? "Range--open " : "")
             }
-         }}
-      >
-         <span className={"Range__title"}>
-            {!fromValue && !beforeValue ? "Created" : ""}
-            {fromValue} {beforeValue}
-         </span>
-         {(fromValue || beforeValue) && (
-            <div
-               className="Range__remove"
-               onClick={onReset}
-               onKeyPress={(e) => {
-                  if (e.key === " " || e.key === "Enter") {
-                     onReset(e);
-                  }
-               }}
-               tabIndex={0}
-            >
-               <svg className="icon__remove">
-                  <use href={`${svg}#remove`}></use>
+            tabIndex={0}
+            onClick={isOpen ? hideMenu : openMenu}
+            onKeyPress={(e) => {
+               if ((e.key === " " && isOpen) || (e.key === "Enter" && isOpen)) {
+                  hideMenu();
+               } else {
+                  openMenu();
+               }
+            }}
+         >
+            <span className={"Range__title"}>
+               {!fromValue && !beforeValue ? "Created" : ""}
+               {fromValue} {beforeValue}
+            </span>
+            {(fromValue || beforeValue) && (
+               <div
+                  className="Range__remove"
+                  onClick={onReset}
+                  onKeyPress={(e) => {
+                     if (e.key === " " || e.key === "Enter") {
+                        onReset(e);
+                     }
+                  }}
+                  tabIndex={0}
+               >
+                  <svg className="icon__remove">
+                     <use href={`${svg}#remove`}></use>
+                  </svg>
+               </div>
+            )}
+            <div className="Range__arrow">
+               <svg className="icon__arrow">
+                  <use href={`${svg}#arrow`}></use>
                </svg>
             </div>
-         )}
-         <div className="Range__arrow">
-            <svg className="icon__arrow">
-               <use href={`${svg}#arrow`}></use>
-            </svg>
-         </div>
-         {isOpen && (
-            <div
-               className={
-                  "Range__сontainer " +
-                  (isOpen ? "Range__сontainer--open " : "")
-               }
-               onClick={(e) => e.stopPropagation()}
-            >
-               <div className="Range__inner">
-                  <input
-                     className="Range__input"
-                     type="number"
-                     name="from"
-                     id="from"
-                     placeholder="from"
-                     onChange={(e) => onChangeFrom(e.target.value)}
-                     value={fromValue}
-                     onKeyPress={(e) => {
-                        if (e.key === "Enter") {
-                           const trimmedValue = e.target.value.trim();
-                           onRequest({ created_gte: trimmedValue || null });
-                        }
-                     }}
-                  />
-                  <hr />
-                  <input
-                     className="Range__input"
-                     type="number"
-                     name="before"
-                     id="before"
-                     placeholder="before"
-                     onChange={(e) => onChangeBefore(e.target.value)}
-                     value={beforeValue}
-                     onKeyPress={(e) => {
-                        if (e.key === "Enter") {
-                           const trimmedValue = e.target.value.trim();
-                           onRequest({ created_lte: trimmedValue || null });
-                        }
-                     }}
-                  />
+            {isOpen && (
+               <div
+                  className={
+                     "Range__сontainer " +
+                     (isOpen ? "Range__сontainer--open " : "")
+                  }
+                  onClick={(e) => e.stopPropagation()}
+               >
+                  <div className="Range__inner">
+                     <input
+                        className="Range__input"
+                        type="number"
+                        name="from"
+                        id="from"
+                        placeholder="from"
+                        onChange={(e) => onChangeFrom(e.target.value)}
+                        value={fromValue}
+                        onKeyPress={(e) => {
+                           if (e.key === "Enter") {
+                              const trimmedValue = e.target.value.trim();
+                              onRequest({ created_gte: trimmedValue || null });
+                           }
+                           if (e.key === " ") e.stopPropagation();
+                        }}
+                     />
+                     <hr />
+                     <input
+                        className="Range__input"
+                        type="number"
+                        name="before"
+                        id="before"
+                        placeholder="before"
+                        onChange={(e) => onChangeBefore(e.target.value)}
+                        value={beforeValue}
+                        onKeyPress={(e) => {
+                           if (e.key === "Enter") {
+                              const trimmedValue = e.target.value.trim();
+                              onRequest({ created_lte: trimmedValue || null });
+                           }
+                           if (e.key === " ") e.stopPropagation();
+                        }}
+                     />
+                  </div>
                </div>
-            </div>
-         )}
+            )}
+         </div>
       </div>
    );
 };
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -24,99 +24,105 @@ const Select = ({
    useOutsideClick(ref, toggleOpen);
 
    return (
-      <div
-         ref={isOpen ? ref : null}
-         className={
-            (className ? className : "") +
-            "Select " +
-            (isOpen ? "Select--open " : "")
-         }
-         onClick={() => {
-            if (!disabled) {
-               if (shouldLog.current && options.length === 0) {
-                  shouldLog.current = false;
-                  onClick();
-               }
-               toggleOpen();
+      <div className="Select__wrapper">
+         <div
+            ref={isOpen ? ref : null}
+            className={
+               (className ? className : "") +
+               "Select " +
+               (isOpen ? "Select--open " : "")
             }
-         }}
-         onKeyPress={(e) => {
-            if (
-               (e.key === " " && !disabled) ||
-               (e.key === "Enter" && !disabled)
-            ) {
-               if (shouldLog.current && options.length === 0) {
-                  shouldLog.current = false;
-                  onClick();
-               }
-               toggleOpen();
-            }
-         }}
-         tabIndex={0}
-      >
-         <span className="Select__title">{value ? value : initialValue}</span>
-         {value && (
-            <div
-               className="Select__remove"
-               onClick={onReset}
-               onKeyPress={(e) => {
-                  if (
-                     (e.key === " " && !disabled) ||
-                     (e.key === "Enter" && !disabled)
-                  ) {
-                     onReset(e);
+            onClick={() => {
+               if (!disabled) {
+                  if (shouldLog.current && options.length === 0) {
+                     shouldLog.current = false;
+                     onClick();
                   }
-               }}
-               tabIndex={0}
-            >
-               <svg className="icon__remove">
-                  <use href={`${svg}#remove`}></use>
-               </svg>
-            </div>
-         )}
-         <div className="Select__arrow">
-            <svg className="icon__arrow">
-               <use href={`${svg}#arrow`}></use>
-            </svg>
-         </div>
-         {isOpen && options && (
-            <ul
-               className={
-                  "Select__optionContainer " +
-                  (isOpen ? "Select__optionContainer--open " : "")
+                  toggleOpen();
                }
-            >
-               <SimpleBar style={{ maxHeight: "inherit" }}>
-                  {Spinner && process === "loading" ? (
-                     <Spinner width="50" height="50" />
-                  ) : ErrorMessage && process === "error" ? (
-                     <ErrorMessage />
-                  ) : options.length === 0 ? (
-                     <span>No options yet.</span>
-                  ) : (
-                     Object.keys(options).map((key) => {
-                        return (
-                           <li
-                              onClick={() => onChange(options[key])}
-                              onKeyPress={(e) => {
-                                 if (e.key === " " || e.key === "Enter") {
-                                    onChange(options[key]);
-                                 }
-                              }}
-                              className={"Select__option "}
-                              key={options[key].id}
-                              tabIndex={0}
-                           >
-                              <p className="Select__optionName">
-                                 {options[key].option}
-                              </p>
-                           </li>
-                        );
-                     })
-                  )}
-               </SimpleBar>
-            </ul>
-         )}
+            }}
+            onKeyPress={(e) => {
+               if (
+                  (e.key === " " && !disabled) ||
+                  (e.key === "Enter" && !disabled)
+               ) {
+                  if (shouldLog.current && options.length === 0) {
+                     shouldLog.current = false;
+                     onClick();
+                  }
+                  toggleOpen();
+               }
+            }}
+            tabIndex={0}
+         >
+            <span className="Select__title">
+               {value ? value : initialValue}
+            </span>
+            <div className="Select__icons">
+               {value && (
+                  <div
+                     className="Select__remove"
+                     onClick={onReset}
+                     onKeyPress={(e) => {
+                        if (
+                           (e.key === " " && !disabled) ||
+                           (e.key === "Enter" && !disabled)
+                        ) {
+                           onReset(e);
+                        }
+                     }}
+                     tabIndex={0}
+                  >
+                     <svg className="icon__remove">
+                        <use href={`${svg}#remove`}></use>
+                     </svg>
+                  </div>
+               )}
+               <div className="Select__arrow">
+                  <svg className="icon__arrow">
+                     <use href={`${svg}#arrow`}></use>
+                  </svg>
+               </div>
+            </div>
+            {isOpen && options && (
+               <ul
+                  className={
+                     "Select__optionContainer " +
+                     (isOpen ? "Select__optionContainer--open " : "")
+                  }
+               >
+                  <SimpleBar style={{ maxHeight: "inherit" }}>
+                     {Spinner && process === "loading" ? (
+                        <Spinner width="50" height="50" />
+                     ) : ErrorMessage && process === "error" ? (
+                        <ErrorMessage />
+                     ) : options.length === 0 ? (
+                        <span>No options yet.</span>
+                     ) : (
+                        Object.keys(options).map((key) => {
+                           return (
+                              <li
+                                 onClick={() => onChange(options[key])}
+                                 onKeyPress={(e) => {
+                                    if (e.key === " " || e.key === "Enter") {
+                                       onChange(options[key]);
+                                    }
+                                 }}
+                                 className={"Select__option "}
+                                 key={options[key].id}
+                                 tabIndex={0}
+                              >
+                                 <p className="Select__optionName">
+                                    {options[key].option}
+                                 </p>
+                              </li>
+                           );
+                        })
+                     )}
+                  </SimpleBar>
+               </ul>
+            )}
+         </div>
       </div>
    );
 };
